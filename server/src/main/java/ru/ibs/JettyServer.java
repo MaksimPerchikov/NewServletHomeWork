2aae78c9048893881f46a8aa959de20d41566a0a
MaksimchikPerchikov <maksim.perch999@mail.ru>
2021-11-02 04:15:19 +0300

    2

---
+++ server/src/main/java/ru/ibs/JettyServer.java
@@ -0,0 +1,38 @@
+package ru.ibs;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
+import ru.ibs.handler.PingHandler;
+
+import java.io.File;
+
+/**
+ * Hello world!
+ *
+ */
+public class JettyServer{
+    public static void main( String[] args ) throws Exception {
+        Server server = new Server(8080);
+
+        //setServlet(server);
+        setWebappServlet(server);
+
+        server.start();
+        server.join();
+
+    }
+
+    private static void setWebappServlet(Server server) {
+        WebAppContext webApp = new WebAppContext();
+        webApp.setWar(new File("webapp/target/webapp.war").getAbsolutePath());
+        webApp.setContextPath("/web");
+        server.setHandler(webApp);
+    }
+
+    private static void setServlet(Server server){
+        ServletHandler handler = new ServletHandler();
+        handler.addServletWithMapping(PingHandler.class,"/ping");
+        server.setHandler(handler);
+    }
+}
